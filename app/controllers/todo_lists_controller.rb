2c650116ae75e4d006c9ebf2be0a360c2b9d3b16
John Maguire <jmaguire5588@gmail.com>
2016-07-05 19:28:52 -0400

    added controllers for todolist and added column for provider uid to users table

---
+++ app/controllers/todo_lists_controller.rb
@@ -0,0 +1,6 @@
+class TodoListsController < ApplicationController
+
+	def index
+	end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,8 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'home#index'
-
+  root 'todo_lists#index'
+  resources :todo_lists
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
