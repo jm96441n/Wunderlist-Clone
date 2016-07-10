6a3fd3206b145f2acdaa6e7525c5fa42a688551e
John Maguire <jmaguire5588@gmail.com>
2016-07-10 15:13:38 -0400

    added test for todo controller :new

--- spec/controllers/todos_controller_spec.rb
+++ spec/controllers/todos_controller_spec.rb
@@ -3,7 +3,21 @@ require 'rails_helper'
 RSpec.describe TodosController, :type => :controller do
 
 	describe 'anonymous user' do
-		
+		before :each do
+			login_with nil
+		end
+
+	end
+	describe 'logged in user with an existing Todo List' do
+		before :each do
+			u = create(:user)
+			login_with u
+			t_l = create(:todolist, user_id: u.id)
+		end
+		it 'should allow a user to make a new todo for a list' do
+			get :new
+			expect( response ).to render_template( :new )
+		end
 	end
 
 end
