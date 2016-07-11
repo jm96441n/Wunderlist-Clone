40a2224243ca7c0dca3cb0361e46cd0b5add1729
John Maguire <jmaguire5588@gmail.com>
2016-07-10 21:07:53 -0400

    removed show route for todo

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'todo_lists#index'
   resources :todo_lists
-  resources :todos, except: :index
+  resources :todos, except: [:index,:show]
   devise_for :users, :controllers => { 
     :omniauth_callbacks => "users/omniauth_callbacks",
     registrations: 'registrations'
